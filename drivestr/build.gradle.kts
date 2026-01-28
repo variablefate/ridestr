1c80eac19f8d8ac5e4224ebe79137bf92c23b038
variablefate <variablefate@proton.me>
2026-01-27 22:10:25 -0800

    Bump version to 0.1.4
    Bug fixes:
    - Fix HTLC refund failures caused by clock skew between device and mint
    - Prevent locked funds from being marked as lost on refund failure
    - Reduce HTLC locktime from 2 hours to 15 minutes for faster fund recovery

--- drivestr/build.gradle.kts
+++ drivestr/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.drivestr.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
